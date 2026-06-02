2c3661e27879be659e192c48c176ffb5c651d0c9
matheus <matheusm1999@live.com>
2026-06-01 22:40:45 -0300

    feat: inicia o projeto

---
+++ src/main/java/matheus/dev/habito/HabitoApplication.java
@@ -0,0 +1,13 @@
+package matheus.dev.habito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/matheus/dev/habito/HabitoApplicationTests.java
@@ -0,0 +1,13 @@
+package matheus.dev.habito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
